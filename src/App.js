de55f72dc3b6dc67c2852a5f132e03640c8e87ba
tthiagoramalhot <tthiagoramalhot@gmail.com>
2020-10-09 16:12:07 -0300

    App.js mostrando componente select option

--- src/App.js
+++ src/App.js
@@ -1,25 +1,9 @@
 import React from 'react'
-import Button from './Components/Button'
+import SelectOption from './Components/SelectOption'
 
 const App = () => {
-  const save = () => {
-    console.log('Dados salvos!')
-  }
-
-  const remove = () => {
-    console.log('Dados excluidos!')
-  }
-
-  const change = () => {
-    console.log('Dados alterados!')
-  }
-
-  return (
-    <div>
-      <Button action={save} outline >Salvar</Button>
-      <Button action={remove}>Deletar</Button>
-      <Button action={change}>Alterar</Button>
-    </div>
+  return(
+    <SelectOption />
   )
 } 
 
