403af89b3f8ab5fe9677ccab9f2118ff4a61654d
tthiagoramalhot <tthiagoramalhot@gmail.com>
2020-10-09 14:58:07 -0300

    Alterei propriedades do botão

--- src/App.js
+++ src/App.js
@@ -2,13 +2,25 @@ import React from 'react'
 import Button from './Components/Button'
 
 const App = () => {
+  const save = () => {
+    console.log('Dados salvos!')
+  }
+
+  const remove = () => {
+    console.log('Dados excluidos!')
+  }
+
+  const change = () => {
+    console.log('Dados alterados!')
+  }
+
   return (
     <div>
-      <Button text="Salvar" />
-      <Button text="Excluir" />
-      <Button text="Alterar" />
+      <Button action={save} outline >Salvar</Button>
+      <Button action={remove}>Deletar</Button>
+      <Button action={change}>Alterar</Button>
     </div>
   )
-}
+} 
 
 export default App
