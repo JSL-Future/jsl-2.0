d83aa2b02e98e2024596f2f0d60ae630592a96e8
tthiagoramalhot <tthiagoramalhot@gmail.com>
2020-10-13 14:06:49 -0300

    correção na estrutura do componente

--- src/Components/Card/index.js
+++ src/Components/Card/index.js
@@ -3,9 +3,8 @@ import style from './style.module.css'
 
 const card = (props) => {
   return (
-    <div className={style}>
-      <h1>{props.title}</h1>
-      <p>{props.text}</p>
+    <div className={style.card}>
+      {props.children}
     </div>
   )
 }
