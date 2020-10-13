be719bf767539765ac4263719638f948557517c9
tthiagoramalhot <tthiagoramalhot@gmail.com>
2020-10-13 11:59:25 -0300

    excluido comentarios

--- src/App.js
+++ src/App.js
@@ -1,20 +1,10 @@
 import React from 'react'
-// import SelectOption from './Components/SelectOption'
 import Input from './Components/Input'
 
 const App = () => {
 
-  // const options = [
-  //   {name: "Selecione", value: "", hidden: true, disabled: false},
-  //   {name: "Preventiva", value: "", hidden: false, disabled: true},
-  //   {name: "Corretiva", value: "", hidden: false, disabled: false},
-  //   {name: "Estacionamento", value: "", hidden: false, disabled: false}
-  // ]
-
   return(
-    // <SelectOption options={options}/>,
-    <Input children={"Placa"} type={"text"} />
+    <Input label="Placa" type="text" id="text" onChange="" />
   )
-} 
-
+}
 export default App
