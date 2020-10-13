ee517d366a28988b14ef9d21d2754dd613ad3bd5
tthiagoramalhot <tthiagoramalhot@gmail.com>
2020-10-13 09:40:14 -0300

    Add array de objetos

--- src/App.js
+++ src/App.js
@@ -2,8 +2,16 @@ import React from 'react'
 import SelectOption from './Components/SelectOption'
 
 const App = () => {
+
+  const options = [
+    {name: "Selecione", value: "", hidden: true, disabled: false},
+    {name: "Preventiva", value: "", hidden: false, disabled: true},
+    {name: "Corretiva", value: "", hidden: false, disabled: false},
+    {name: "Estacionamento", value: "", hidden: false, disabled: false}
+  ]
+
   return(
-    <SelectOption />
+    <SelectOption options={options}/>
   )
 } 
 
