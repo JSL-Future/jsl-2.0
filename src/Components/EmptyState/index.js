d986e94e1976321e8100bb77ed7827bcdca7693c
tthiagoramalhot <tthiagoramalhot@gmail.com>
2020-10-13 16:24:24 -0300

    adiciondo imagem ao componente

--- src/Components/EmptyState/index.js
+++ src/Components/EmptyState/index.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import style from './style.module.css'
+import imageEmptyState from '../../assets/Images/image-empty-state.svg'
 
 const emptyState = (props) => {
   return (
     <div className={style.emptyState}>
-      <img className={style.emptyStateImg} src=""/>
+      <img className={style.emptyStateImg} src={imageEmptyState}/>
       <h1>{props.title}</h1>
       <p>{props.description}</p>
     </div>
