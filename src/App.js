9ef746d78aaad04c661594bbf1c07fb518102296
tthiagoramalhot <tthiagoramalhot@gmail.com>
2020-10-13 11:37:57 -0300

    adicionado mudanças do arquivo

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import React from 'react'
-import SelectOption from './Components/SelectOption'
+// import SelectOption from './Components/SelectOption'
+import Input from './Components/Input'
 
 const App = () => {
 
-  const options = [
-    {name: "Selecione", value: "", hidden: true, disabled: false},
-    {name: "Preventiva", value: "", hidden: false, disabled: true},
-    {name: "Corretiva", value: "", hidden: false, disabled: false},
-    {name: "Estacionamento", value: "", hidden: false, disabled: false}
-  ]
+  // const options = [
+  //   {name: "Selecione", value: "", hidden: true, disabled: false},
+  //   {name: "Preventiva", value: "", hidden: false, disabled: true},
+  //   {name: "Corretiva", value: "", hidden: false, disabled: false},
+  //   {name: "Estacionamento", value: "", hidden: false, disabled: false}
+  // ]
 
   return(
-    <SelectOption options={options}/>
+    // <SelectOption options={options}/>,
+    <Input children={"Placa"} type={"text"} />
   )
 } 
 
