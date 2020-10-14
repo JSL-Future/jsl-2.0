70ba344fbcc26b9e1a0fe56be2fd14741db41502
tthiagoramalhot <tthiagoramalhot@gmail.com>
2020-10-14 12:19:03 -0300

    alterado css de alguns componentes

--- src/Components/EmptyState/index.js
+++ src/Components/EmptyState/index.js
@@ -5,7 +5,10 @@ import imageEmptyState from '../../assets/Images/image-empty-state.svg'
 const emptyState = (props) => {
   return (
     <div className={style.emptyState}>
-      <img className={style.emptyStateImg} src={imageEmptyState}/>
+      <img className={style.emptyStateImg} 
+        src={imageEmptyState} 
+        alt="sem dados"
+      />
       <h1>{props.title}</h1>
       <p>{props.description}</p>
     </div>
