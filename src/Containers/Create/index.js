d0c3318143e0c43fb38f31feb099a5331b03d196
tthiagoramalhot <tthiagoramalhot@gmail.com>
2020-10-15 11:54:59 -0300

    inclusão de componentes

--- src/Containers/Create/index.js
+++ src/Containers/Create/index.js
@@ -1,26 +1,63 @@
 import React from 'react'
-import { Card, Header, SelectOption } from '../../Components'
+import { Button, Card, Header, Input, SelectOption } from '../../Components'
 import style from './style.module.css'
 
 const createEntry = () => {
-  const listSelect = [
+  const listSelectEvent = [
     {value: "", hidden: true, disabled: false, name: "Selecione uma opção"},
     {value: "0", hidden: false, disabled: false, name: "Preventiva"},
     {value: "1", hidden: false, disabled: false, name: "Corretiva"},
     {value: "2", hidden: false, disabled: false, name: "Estacionamento"},
   ]
+
+  const listSelectOper = [
+    {value: "", hidden: true, disabled: false, name: "Selecione uma opção"},
+    {value: "0", hidden: false, disabled: false, name: "Mercedes-Benz"},
+    {value: "1", hidden: false, disabled: false, name: "Ford Motor"},
+    {value: "2", hidden: false, disabled: false, name: "GM Interplantas"},
+    {value: "2", hidden: false, disabled: false, name: "GM Cegonha"},
+    {value: "2", hidden: false, disabled: false, name: "Lear Corporation"},
+  ]
   
   return(
     <div className={style.container}>
       <Header />
+      
       <Card>
         <h1>Adicionando Implementos</h1>
         <p>Para adicionar novos implementos preencha as informações abaixo:</p>
       </Card>
+      
+      <Card>
+        <h1>Operação</h1>
+        <SelectOption options={listSelectOper}/>
+      </Card>
+      
       <Card>
         <h1>Evento</h1>
-        <SelectOption options={listSelect}/>
+        <SelectOption options={listSelectEvent}/>
       </Card>
+      
+      <Card>
+        <h1>Informações Gerais</h1>
+        <Input 
+          label="Placa"
+          type="text"
+          name="placa"
+          id="textPlaca"
+          onChange=""
+        />
+        <Input 
+          label="Motorista"
+          type="text"
+          name="motorista"
+          id="textMotorista"
+          onChange=""
+        />
+      </Card>
+
+      <Button>Salvar</Button>
+
     </div>
   )
 }
