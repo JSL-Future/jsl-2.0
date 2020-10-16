f289aca0da16784a2f006f1c756f48e0fa8a0c78
tthiagoramalhot <tthiagoramalhot@gmail.com>
2020-10-16 14:01:52 -0300

    add arquivo para exportar componentes

---
+++ src/Components/index.js
@@ -0,0 +1,17 @@
+import Button from './Button'
+import Card from './Card'
+import EmptyState from './EmptyState'
+import Header from './Header'
+import Input from './Input'
+import SelectOption from './SelectOption'
+import Table from './Table'
+
+export {
+  Button,
+  Card,
+  EmptyState,
+  Header,
+  Input,
+  SelectOption,
+  Table,
+}
