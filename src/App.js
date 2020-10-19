33ad405ac9b6a156be159b56a0240bbd00e2fc95
tthiagoramalhot <tthiagoramalhot@gmail.com>
2020-10-19 16:14:40 -0300

    App.js alterado para teste

--- src/App.js
+++ src/App.js
@@ -2,8 +2,14 @@ import React from 'react'
 import Manager from './Containers/Manager'
 
 const App = () => {
+
+  const data = [
+    {placa: "DSG2100", date: "03/11/1990 às 08:00", event: "Preventiva" },
+    {placa: "DSG2100", date: "03/11/1990 às 08:00", event: "Preventiva" },
+  ]
+
   return (
-    <Manager />
+    <Manager data={data} dataLength={data.length}/>
   )
 }
 
