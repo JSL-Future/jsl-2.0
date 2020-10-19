257c096aca7ca55e961aac53b991df7009f367dd
Alexandre Soares <ale_santos.soares@hotmail.com>
2020-10-19 12:21:49 -0300

    chore: update input search

--- src/Components/InputSearch/index.js
+++ src/Components/InputSearch/index.js
@@ -1,17 +1,17 @@
 import React from 'react'
-import Style from './style.module.css'
+import style from './style.module.css'
 import IconSearch from '../../assets/Images/icon-search.svg'
 
 const InputSearch = (props) => {
   return (
-    <div>
-      <img src={IconSearch} alt="Pesquisa"/>
-      <input className={Style}
-        id={props.name}
-        name={props.name} 
+    <div className={style.container}>
+      <img className={style.iconSearch} src={IconSearch} alt="Pesquisa"/>
+      <input
+        className={style.input}
+        name={props.name}
         onChange={props.onchange}
         placeholder={props.placeholder}
-        type={props.type} 
+        type={props.type}
       />
     </div>
   )
