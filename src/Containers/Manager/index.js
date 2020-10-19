84c353020142e201b6a6df6d3530a78fc5ec0b77
tthiagoramalhot <tthiagoramalhot@gmail.com>
2020-10-19 12:25:16 -0300

    alterado condição da tabela

--- src/Containers/Manager/index.js
+++ src/Containers/Manager/index.js
@@ -2,51 +2,33 @@ import React from 'react'
 import { Button, Card, EmptyState, Header, Table } from '../../Components'
 import style from './style.module.css'
 
-const Manager = () => {
-
-  const lineTable = [
-    {placa: "DSG2100", date: "03/11/1990", event:"Preventiva"},
-    {placa: "DSG2100", date: "03/11/1990", event:"Corretiva"},
-    {placa: "DSG2100", date: "03/11/1990", event:"Estacionamento"},
-  ]
-
-  if (lineTable.length > 0) {
-    return (
-      <div className={style.container}>
-        <Header />
-        <Card>
-          <h1>Gerenciamento de implementos</h1>
-          <p>Adicione um novo implemento para a gestão da entrada e saída em cada operação</p>
-          <Button action="" outline>
-            Adicionar
-          </Button>
-        </Card>
-        <Card>
-          <Table line={lineTable} />
-        </Card>
-      </div>
-    )
-  } else {
-    return (
-      <div className={style.container}>
-        <Header />
-        <Card>
-          <h1>Gerenciamento de implementos</h1>
-          <p>Adicione um novo implemento para a gestão da entrada e saída em cada operação</p>
-          <Button action="" outline>
-            Adicionar
-          </Button>
-        </Card>
-        <Card>
-          <EmptyState
-            title="Não temos nenhum implemento criado"
-            description="Para adicionar um novo implmento clique em 
-            “Adicionar” na parte superior!"
-          />
-        </Card>
-      </div>
-    )
-  }
+const Manager = (props) => {
+  return (
+    <div className={style.container}>
+      <Header />
+      <Card>
+        <h1>Gerenciamento de implementos</h1>
+        <p>Adicione um novo implemento para a gestão da entrada e saída em cada operação</p>
+        <Button action={props.addImplement} outline>
+          Adicionar
+        </Button>
+      </Card>
+      <Card>
+        {props.data.length > 0 
+          ? (
+            <Table data={props.data} />
+          )
+          : (
+            <EmptyState
+              title="Não temos nenhum implemento criado"
+              description="Para adicionar um novo implmento clique em 
+              “Adicionar” na parte superior!"
+            />
+          )
+        }
+      </Card>
+    </div>
+  )
 }
 
 export default Manager
