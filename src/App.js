7f513e4a704489fd439cebc626a67a66b4c54394
tthiagoramalhot <tthiagoramalhot@gmail.com>
2020-10-20 09:08:23 -0300

    Alterado para teste

--- src/App.js
+++ src/App.js
@@ -4,8 +4,10 @@ import Manager from './Containers/Manager'
 const App = () => {
 
   const data = [
-    {placa: "DSG2100", date: "03/11/1990 às 08:00", event: "Preventiva" },
-    {placa: "DSG2100", date: "03/11/1990 às 08:00", event: "Preventiva" },
+    {placa: "DSG2100", date: "03/11/1990 às 08:00", event: "Preventiva" , permanence: "80 horas", preventiva: "preventiva", corretiva: false, estacionamento: false},
+    {placa: "DSG2100", date: "03/11/1990 às 08:00", event: "Corretiva" , permanence: "80 horas", preventiva: false, corretiva: true, estacionamento: false},
+    {placa: "DSG2100", date: "03/11/1990 às 08:00", event: "Estacionamento" , permanence: "80 horas", preventiva: false, corretiva: false, estacionamento: true},
+    {placa: "DSG2100", date: "03/11/1990 às 08:00", event: "Preventiva" , permanence: "80 horas", preventiva: true, corretiva: false, estacionamento: false},
   ]
 
   return (
