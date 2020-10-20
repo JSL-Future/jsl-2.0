bf53d4ed3a10d85f90a1d04593e729c2b42dd379
tthiagoramalhot <tthiagoramalhot@gmail.com>
2020-10-20 15:47:28 -0300

    Ajustado estrutura props

--- src/Containers/Create/index.js
+++ src/Containers/Create/index.js
@@ -2,23 +2,7 @@ import React from 'react'
 import { Button, Card, Header, Input, SelectOption } from '../../Components'
 import style from './style.module.css'
 
-const createEntry = () => {
-  const listSelectEvent = [
-    {value: "", hidden: true, disabled: false, name: "Selecione uma opção"},
-    {value: "0", hidden: false, disabled: false, name: "Preventiva"},
-    {value: "1", hidden: false, disabled: false, name: "Corretiva"},
-    {value: "2", hidden: false, disabled: false, name: "Estacionamento"},
-  ]
-
-  const listSelectOper = [
-    {value: "", hidden: true, disabled: false, name: "Selecione uma opção"},
-    {value: "0", hidden: false, disabled: false, name: "Mercedes-Benz"},
-    {value: "1", hidden: false, disabled: false, name: "Ford Motor"},
-    {value: "2", hidden: false, disabled: false, name: "GM Interplantas"},
-    {value: "2", hidden: false, disabled: false, name: "GM Cegonha"},
-    {value: "2", hidden: false, disabled: false, name: "Lear Corporation"},
-  ]
-  
+const createEntry = (props) => {  
   return(
     <div className={style.container}>
       <Header />
@@ -31,12 +15,12 @@ const createEntry = () => {
       
       <Card>
         <h1>Operação</h1>
-        <SelectOption options={listSelectOper} />
+        <SelectOption options={props.dataListOper} />
       </Card>
       
       <Card>
         <h1>Evento</h1>
-        <SelectOption options={listSelectEvent} />
+        <SelectOption options={props.dataListEvent} />
       </Card>
       
       <Card>
@@ -57,7 +41,11 @@ const createEntry = () => {
         />
       </Card>
 
-      <Button>Salvar</Button>
+      <Button
+        action={props.save}
+      >
+        Salvar
+      </Button>
 
     </div>
   )
