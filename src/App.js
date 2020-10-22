caa3a21609bd134c52e07a0398c56f121b9b3115
tthiagoramalhot <tthiagoramalhot@gmail.com>
2020-10-22 09:26:35 -0300

    Alterado para teste

--- src/App.js
+++ src/App.js
@@ -2,8 +2,16 @@ import React from 'react'
 import Release from './Containers/Release'
 
 const App = () => {
+
+  const checkin = {
+    plate: "DSG2100",
+    event: "Corretiva",
+    createdAt: "02/10/2020 às 08:00",
+    updatedAt: ""
+  }
+
   return (
-    <Release />
+    <Release checkin={checkin} />
   )
 }
 
