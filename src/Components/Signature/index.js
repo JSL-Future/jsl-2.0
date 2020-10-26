b4e08e83310d085007793bc46476cc0ecc2a253d
tthiagoramalhot <tthiagoramalhot@gmail.com>
2020-10-26 09:16:53 -0300

    Criado componente Signature

---
+++ src/Components/Signature/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+// import Style from './style.module.css'
+import SignaturePad from 'react-signature-pad-wrapper'
+
+const Signature = () => {
+  return (
+    <SignaturePad
+      
+    />
+  )
+}
+
+export default Signature
