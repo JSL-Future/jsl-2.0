0d42b233b23b543aa99e870ae88a5c90ae06b583
tthiagoramalhot <tthiagoramalhot@gmail.com>
2020-11-25 15:51:58 -0300

    component-select-option

--- src/Components/SelectOption/index.js
+++ src/Components/SelectOption/index.js
@@ -2,27 +2,30 @@ import React from 'react'
 import Style from './style.module.css'
 
 const selectOption = (props) => {
-  return(
-    <select
-      className={Style.selectOptions}
-      value={props.selectValue}
-      name={props.selectName}
-      onChange={props.onchange}
-    >
-      {
-        props.options.map((option, index) => (
-          <option
-            key={index}
-            value={option.value}
-            hidden={option.hidden}
-            disabled={option.disabled}
-          >
-            {option.name}
-          </option>
+  return (
+    <label className={Style.label}>
+      {props.label}
+      <select
+        className={Style.selectOptions}
+        value={props.selectValue}
+        name={props.selectName}
+        onChange={props.onchange}
+      >
+        {
+          props.options.map((option, index) => (
+            <option
+              key={index}
+              value={option.value}
+              hidden={option.hidden}
+              disabled={option.disabled}
+            >
+              {option.name}
+            </option>
           )
-        )
-      }
-    </select>
+          )
+        }
+      </select>
+    </label>
   )
 }
 
