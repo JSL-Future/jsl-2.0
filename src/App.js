1073163408fbf1543bfa033739b9a42388068c7a
Alexandre Soares <ale_santos.soares@hotmail.com>
2020-11-26 00:11:02 -0300

    chore: remove file not used

--- src/App.js
+++ src/App.js
@@ -2,9 +2,7 @@ import React from 'react'
 import { Route, Switch, Redirect } from 'react-router-dom'
 import Manager from './Pages/Manager'
 import Create from './Pages/Create'
-import Signature from './Pages/Signature'
 import Details from './Pages/Details'
-import Release from './Pages/Release'
 import Login from './Pages/Login'
 
 const App = () => {
@@ -13,9 +11,7 @@ const App = () => {
       <Route path='/auth' component={Login} />
       <Route path='/manager' component={Manager} />
       <Route path='/create' component={Create} />
-      <Route path='/signature' component={Signature} />
       <Route path='/detail/:id' component={Details} />
-      <Route path='/release/:id' component={Release} />
       <Redirect from="*" to="/auth" />
     </Switch>
   )
