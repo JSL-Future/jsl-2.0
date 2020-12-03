5cbb6dfd780d16fbee154a88eaf6fdaca48e881d
Alexandre Soares <ale_santos.soares@hotmail.com>
2020-12-03 13:20:25 -0300

    fix diff date

--- src/utils/parserDate.js
+++ src/utils/parserDate.js
@@ -4,11 +4,40 @@ import 'moment/locale/pt-br'
 const formattedDate = date => moment(date).format('DD/MMM HH:mm')
 
 const diff = (createdAt, updatedAt, status) => {
-  const start = moment(createdAt)
-  const end = status !== 'check-out' ? moment() : moment(updatedAt)
-  const diff = end.diff(start)
-  const diffTime = moment.utc(diff).format('HH:mm')
-  return diffTime
+  const lastTime = status === 'check-out' ? updatedAt : new Date()
+  let d = (new Date(lastTime)) - (new Date(createdAt));
+  let weekdays     = Math.floor(d/1000/60/60/24/7);
+  let days         = Math.floor(d/1000/60/60/24 - weekdays*7);
+  let hours        = Math.floor(d/1000/60/60    - weekdays*7*24            - days*24);
+  let minutes      = Math.floor(d/1000/60       - weekdays*7*24*60         - days*24*60         - hours*60);
+
+  return messageDates({
+    weekdays,
+    days,
+    hours,
+    minutes,
+  })
+}
+
+const messageDates = ({ weekdays, days, hours, minutes }) => {
+  let message = 'Há,'
+  if (weekdays > 0) {
+    message += `${weekdays},${weekdays > 1 ? 'semanas' : 'semana'},`
+  }
+
+  if (days > 0) {
+    message += `${days},${days > 1 ? 'dias' : 'dia'},`
+  }
+
+  if (hours) {
+    message += `${hours.toString().length === 1 ? '0'+ hours : hours}h`
+  }
+
+  if (minutes) {
+    message += `${minutes.toString().length >= 2 ? minutes : '0'+ minutes}m`
+  }
+
+  return message.replace(/,/gi, ' ')
 }
 
 export {
